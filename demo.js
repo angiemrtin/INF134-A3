fd9dcad59de913d793d59b4c82e435f46263dd20
angiemrtin <angelaim@uci.edu>
2021-05-01 22:00:53 -0700

    added files, linked js to html

--- demo.js
+++ demo.js
@@ -2,6 +2,7 @@ import {MyToolkit} from './mytoolkit.js';
 
 // Implement a MyToolkit Button
 var btn = new MyToolkit.Button;
+//document.getElementById("button").display("block");
 btn.move(100,100);
 btn.onclick(function(e){
 	console.log(e);
--- mytoolkit.js
+++ mytoolkit.js
@@ -3,20 +3,20 @@ import {SVG} from './svg.min.js';
 var MyToolkit = (function() {
     var Button = function(){
         var draw = SVG().addTo('body').size('100%','100%');
-        var rect = draw.rect(100,50).fill('red')
+        var rect = draw.rect(100,50).fill('#996633')
         var clickEvent = null
 
         rect.mouseover(function(){
-            this.fill({ color: 'blue'})
+            this.fill({ color: 'skyblue'})
         })
         rect.mouseout(function(){
-            this.fill({ color: 'red'})
+            this.fill({ color: '#996633'})
         })
         rect.mouseup(function(){
-            this.fill({ color: 'red'})
+            this.fill({ color: '#996633'})
         })
         rect.click(function(event){
-            this.fill({ color: 'pink'})
+            this.fill({ color: '#996633'})
             if(clickEvent != null)
                 clickEvent(event)
         })
