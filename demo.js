6e47b6f964a778c29e565a2a972c135c97538569
angiemrtin <angelaim@uci.edu>
2021-05-18 13:23:35 -0700

    added Widgets and Demo code
    - added Button, Radio Button, Checkbox, and Progress Bar widget
    - added demo code to place widgets on page
    - added JsDoc comments

--- demo.js
+++ demo.js
@@ -1,9 +1,33 @@
 import {MyToolkit} from './mytoolkit.js';
 
-// Implement a MyToolkit Button
-var btn = new MyToolkit.Button;
 
-btn.move(100,100);
+//Button
+var btn = new MyToolkit.Button;
+btn.setId("button1");
+btn.label("hello!");
+btn.move(50,50);
 btn.onclick(function(e){
-	console.log(e);
+	console.log("click at " + e.target.id)
 });
+
+//Checkbox
+var checkbox = new MyToolkit.Checkbox;
+checkbox.move(200, 60);
+checkbox.label("hello checkbox");
+
+//Radio Buttons
+var radioGroup = new MyToolkit.RadioButton(4);
+radioGroup.move(50, 150);
+radioGroup.label("change me!");
+radioGroup.onclick(function(e){
+	console.log("click at " + e.target.id)
+});
+
+//Progress Bar
+var progressBar = new MyToolkit.ProgressBar;
+progressBar.setBarWidth(300);
+progressBar.move(300, 150);
+progressBar.setProgressWidth(25);
+progressBar.getValue();
+progressBar.incrementValue(25);
+progressBar.getValue();
